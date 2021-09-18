e7b2d4e138f05163e68f51673ad752c96bcc2269
David Delgadillo <daviddel.731@gmail.com>
2021-09-18 09:22:40 -0700

    Product component built  and product feed component almost done

--- src/components/ProductFeed.jsx
+++ src/components/ProductFeed.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import Product from './Product';
 
 function ProductFeed({ products }) {
   return (
-    <div>
-      <h1>Products here!!</h1>
-      {products.map(product => (
-        
-        <p>{product.title}</p>
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:gridcols-4 md:-mt-52 mx-auto">
+      {products.map(({ id, title, price, description, category, image }) => (
+        <Product 
+          key={id}
+          id={id}
+          title={title}
+          price={price}
+          description={description}
+          category={category}
+          image={image}
+        />
       ))}
     </div>
   );
