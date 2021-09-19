025435acac841b71959c2ca38bf98dadeaabeac2
David Delgadillo <daviddel.731@gmail.com>
2021-09-18 22:22:23 -0700

    checkout page complete and added delete functionality and subtle style changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,16 +6,21 @@ import {
   ShoppingCartIcon
 } from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from "next-auth/client";
+import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
+import { selectItems } from '../slices/basketSlice';
 
 function Header() {
   const [session] = useSession();
+  const router = useRouter();
+  const items = useSelector(selectItems)
 
-  
   return (
     <header>
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
+          onClick={() => router.push('/')}
             src='https://links.papareact.com/f90'
             width={150}
             height={40}
@@ -24,9 +29,16 @@ function Header() {
           />
         </div>
 
-        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
-          <input className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" type="text" />
-          <SearchIcon className="h-12 p-4" />
+        <div 
+          className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500"
+        >
+          <input 
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" 
+            type="text" 
+          />
+          <SearchIcon 
+            className="h-12 p-4" 
+          />
         </div>
 
         {/* Right */}
@@ -50,9 +62,13 @@ function Header() {
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
 
-          <div className="relative link flex items-center">
+          <div onClick={() => router.push('/checkout')} className="relative link flex items-center">
 
-            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+            <span 
+              className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold"
+            >
+              {items.length}
+            </span>
 
             <ShoppingCartIcon className="h-10"/>
             <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
@@ -61,7 +77,7 @@ function Header() {
         </div>
       </div>
 
-      <div className="flex item-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+      <div className="flex item-center space-x-7 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
         <p className="link flex items-center">
           <MenuIcon className="h-6 mr-1"/>
           All
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,15 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
+import { store } from '../app/store';
 import { Provider } from 'react-redux';
 import { Provider as AuthProvider } from "next-auth/client";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     </AuthProvider>
   )}
 
