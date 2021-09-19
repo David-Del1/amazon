5115138b2cab50ae97a0439a34b1cd66568d361e
David Delgadillo <daviddel.731@gmail.com>
2021-09-19 11:08:07 -0700

    Added display in checkout to provide total for items in basket

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -4,9 +4,16 @@ import Image from 'next/image';
 import { useSelector } from 'react-redux';
 import { selectItems } from '../slices/basketSlice';
 import CheckoutProduct from '../components/CheckoutProduct';
+import { useSession } from 'next-auth/client';
+import { selectTotal } from '../slices/basketSlice';
+import CurrencyFormat from 'react-currency-format';
 
 function Checkout() {
   const items = useSelector(selectItems)
+  const [session] = useSession();
+  const total = useSelector(selectTotal)
+
+
   return (
     <div className="bg-gray-100">
     <Header />
@@ -23,7 +30,7 @@ function Checkout() {
           <h1 
             className="text-3xl border-b pb-4"
           >
-            {items.length ? 'Shopping Basket' : 'Your Cart is empty.'}
+            {items.length ? 'Your Shopping Cart' : 'Your Cart is empty.'}
           </h1>
 
           {items.map((item, i) => (
@@ -42,7 +49,29 @@ function Checkout() {
         </div>
       </div>
 
-      <div></div>
+      <div>
+        {items.length > 0 && (
+          <div className="flex flex-col bg-white p-10 shadow-md">
+            <h2 className="whitespace-nowrap">
+              Subtotal ({items.length} items):{" "}
+              <span className="font-bold">
+                <CurrencyFormat
+                  value={total} 
+                  prefix={'$'}
+                  displayType={'text'}
+                  decimalScale={2}
+                  fixedDecimalScale={true}
+                />
+              </span>
+            </h2>
+
+            <button className={`button mt-2 ${!session && 'from-gray-400 to-gray-500 border-gray-300 cursor-not-allowed'
+            }`}>
+              {!session ? 'Sign In to Checkout' : "Checkout"}
+            </button>
+          </div>
+        )}
+      </div>
     </main>
     </div>
   );
